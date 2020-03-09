555b8e63404c2f1fb4216678da361aa4a018e3a2
JayJatoba <jguilhermejatoba@hotmail.com>
2020-03-09 08:37:32 -0300

    Término exercícios

--- Linguagens1_Projetos/Pratica01/Lista 1/Ex3.java
+++ Linguagens1_Projetos/Pratica01/Lista 1/Ex3.java
@@ -11,12 +11,20 @@ public class Ex3 {
 
         System.out.println("Informe o nome de usuário: ");
         user = scanner.nextLine();
+
+        while (!user.contentEquals("18.01790-8@maua.br")){
+            System.out.println("Usuário não reconhecido. Coloque o usuário certo.");
+            user = scanner.nextLine();
+        }
+
         System.out.println("Informe sua senha: ");
         key = scanner.nextLine();
-        
-        
-        
-        
+        while (!key.contentEquals("551689031")){
+            System.out.println("Senha incorreta. Coloque a senha certa.");
+            key = scanner.nextLine();
+        }
+
+        System.out.println("Você está logado.);
 
         
 
--- Linguagens1_Projetos/Pratica01/Lista 1/Ex7.java
+++ Linguagens1_Projetos/Pratica01/Lista 1/Ex7.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 
 public class Ex7 {
     public static void main(String[] args) {
         // Exercicio 7
         
-        Integer num=-1;
+        Integer num=-1, sum=0, n;
+        
         
         Scanner scanner = new Scanner(System.in);
 
+
         
         while (num<=9){
-            System.out.println("Informe o valor inicial da sequência: ");
+            System.out.println("Informe o valor para somar os algarismos (>=10): ");
             num = scanner.nextInt();
         }
-        
-        
+        while (num > 0){
+            n = num % 10;
+            sum = sum + n;
+            num = num / 10;
+        }
+        System.out.println("A soma dos algarismos é: " + sum);
     }
 } 
