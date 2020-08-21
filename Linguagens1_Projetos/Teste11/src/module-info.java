7c8b87c6d41aed1c9cdd0395aac87f3f985f458b
JayJatoba <jguilhermejatoba@hotmail.com>
2020-08-21 00:37:54 -0300

    Java FX funcionando - IntelliJ

---
+++ Linguagens1_Projetos/Teste11/src/module-info.java
@@ -0,0 +1,13 @@
+module Teste11 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+
+    opens sample;
+}
