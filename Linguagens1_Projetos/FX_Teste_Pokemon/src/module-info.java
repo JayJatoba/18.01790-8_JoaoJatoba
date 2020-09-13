491c14cd5c31e782ed4f94a8daae5831645e3da0
JayJatoba <jguilhermejatoba@hotmail.com>
2020-09-12 21:23:31 -0300

    Update FX
    Aprendendo JavaFX

---
+++ Linguagens1_Projetos/FX_Teste_Pokemon/src/module-info.java
@@ -0,0 +1,8 @@
+module FX_Teste_Pokemon {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+
+
+    opens sample;
+}
---
+++ Linguagens1_Projetos/FX_Teste_Pokemon/src/sample/model/Pokemon.java
@@ -0,0 +1,9 @@
+package sample.model;
+
+public class Pokemon {
+    private String urlImage;
+    private int id;
+    private String name;
+    private String rarity;
+
+}
